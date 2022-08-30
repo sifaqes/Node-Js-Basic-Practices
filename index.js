d5267015839707ea5f06e33fc6a19e8e705a73b7
SIFAQES ZERROUKI <zs7@gcloud.ua.es>
2022-08-30 02:55:41 +0200

    segundo

---
+++ index.js
@@ -0,0 +1,11 @@
+// Servidor HTTP
+const http = require('http');
+
+const handleServer = function(req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.write('<h1 style="color:blue;">Salam</h1>');
+    res.end();
+}
+
+const server = http.createServer(handleServer);
+server.listen(3000, function() { console.log('Server on  port  3000') });
